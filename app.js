9dc9cd9ac91126fbd6a5970e20ae6c64cf130b62
yaseen1998 <>
2021-06-30 12:40:28 +0000

    website

---
+++ app.js
@@ -0,0 +1,16 @@
+alert ("thank you to visit my first website I hope you feel comfortable with it")
+var name = prompt("select your favanime(choose the number)"+"\n"+"1.death note"+"\n"+"2.Attack on titan"+"\n"+"3.Fullmetal Alchemist" +"\n"+"4.Sword Art Online")
+if (name == 1){
+  document.write("<h3> your favanime :death note</h3>"+"\n"+"<img"+ " src="+"https://static3.srcdn.com/wordpress/wp-content/uploads/2021/03/Death-Note-Characters-Ranked-by-Likability-Feature-Image-2.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5"+">")}
+  else if (name ==2 ){
+    document.write("<h3>your favanime :Attack on titan</h3>"+"\n"+"<img"+ " src="+"https://an-master.net/wp-content/uploads/2020/11/Attack-on-Titan-Season-4-Release-Date-Trailer-News.jpg"+">")}
+    else if (name ==3 ){
+    document.write("<h3>your favanime :Fullmetal Alchemist</h3>"+"\n"+"<img"+ " src="+"https://thefandomentals.com/wp-content/uploads/2018/11/Webp.net-resizeimage.jpg"+">")}
+    else if (name ==4 ){
+    document.write("<h3>your favanime :Sword Art Online</h3>"+"\n"+"<img"+ " src="+"https://i.pinimg.com/originals/a6/d9/6b/a6d96b1e585781865bc6b4fe8d97a50f.jpg"+">")}
+
+else {
+    document.write("<h3>you don't choose any one </h3>"+"\n"+"<img"+ " src="+"https://store-images.s-microsoft.com/image/apps.3120.13911809384204628.e1b36730-e00b-4750-9bcd-f69d990b087c.f98b9ae8-d692-4c0c-aff5-fbe02f9459a9?mode=scale&q=90&h=720&w=1280&background=%23FFFFFF"+">")}
+   
+
+
